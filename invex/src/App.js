5d73fdc5f06cc6a83be00bf3fbac9b49ef40f255
Gurupriyan <gurupriyan98@gmail.com>
2022-12-05 18:27:35 -0500

    added routes to home, login and signup

--- invex/src/App.js
+++ invex/src/App.js
@@ -1,9 +1,17 @@
-import './App.css';
+import { Routes, Route } from 'react-router-dom'
+import LandingPage from './pages/landing-page/Landing-page';
+import LoginPage from './pages/login-page/Login-page';
+import Register from './pages/register-page/Register';
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>INVEX</h1>
+    <div>
+      <Routes>
+        <Route path='/' element= {<LandingPage />} />
+        <Route path='/login' element = {<LoginPage />} />
+        <Route path='/signup' element = {<Register />} />
+      </Routes>
     </div>
   );
 }
