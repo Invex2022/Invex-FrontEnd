d8159a833093eac21b16b93e2b2c1b30b6c6b34d
Gurupriyan <gurupriyan98@gmail.com>
2022-12-13 23:53:38 -0500

    finished initial design of sign up page

--- invex/src/pages/register-page/Register.jsx
+++ invex/src/pages/register-page/Register.jsx
@@ -1,8 +1,80 @@
+import { useState } from 'react'
+import { Button, TextField, Typography } from '@mui/material'
+import Navbar from '../../components/Navbar/Navbar'
+import getStarted from '../../assets/example-29.svg'
+import './Register.css'
+
+
 const Register = () => {
+    const [firstName, setFirstName] = useState('')
+    const [lastName, setLastName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
     return (
-        <div>
-            Sign Up
-        </div>
+        <>
+            <Navbar />
+            <div className='form-section'>
+                <div className='first-section'>
+                    <img src = {getStarted} alt='login' className='login-pic'/>
+                </div>
+
+                <div className='second-section'>
+                    <Typography
+                        variant = "h3"
+                    >
+                        Create a new account.
+                    </Typography>
+                    <Typography
+                        variant = "h6"
+                    >
+                        Already a member? 
+                    </Typography>
+
+                    <div>
+                        <TextField
+                            id="standard-basic" 
+                            label="First Name" 
+                            variant="standard"
+                            onChange={(e) => setFirstName(e.target.value)}
+                            sx={{margin:2}}
+                        />
+                        <TextField
+                            id="standard-basic" 
+                            label="Last Name" 
+                            variant="standard"
+                            onChange={(e) => setLastName(e.target.value)}
+                            sx={{margin:2}}
+                        />
+                    </div>
+                    <div>
+                        <TextField
+                            id="standard-basic" 
+                            label="Email" 
+                            variant="standard"
+                            onChange={(e) => setEmail(e.target.value)}
+                            sx={{margin:2}}
+                        />
+                        <TextField
+                            id="standard-basic" 
+                            label="Password"
+                            type= "password" 
+                            variant="standard"
+                            onChange={(e) => setPassword(e.target.value)}
+                            sx={{margin:2}}
+                        />
+                    </div>
+                    <Button 
+                        variant="contained" sx={{ 
+                        backgroundColor: '#24387C', 
+                        margin : 3,
+                        "&:hover" : {
+                            backgroundColor : '#6C63FF'
+                        }
+                    }}>Create your account</Button>
+                </div>
+            </div>
+        </>
     )
 }
 
