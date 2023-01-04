41718109e636efcd28b69ac3dd4db44f139395b9
xperience001 <tobilobatijani2011@gmail.com>
2023-01-04 03:27:31 +0100

    about-page-in-progress

--- invex/src/App.js
+++ invex/src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import LandingPage from './pages/landing-page/Landing-page';
 import LoginPage from './pages/login-page/Login-page';
 import Register from './pages/register-page/Register';
+import About from './pages/about-page/About';
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Route path='/' element= {<LandingPage />} />
         <Route path='/login' element = {<LoginPage />} />
         <Route path='/signup' element = {<Register />} />
+        <Route path='/about' element = {<About />} />
       </Routes>
     </div>
   );
